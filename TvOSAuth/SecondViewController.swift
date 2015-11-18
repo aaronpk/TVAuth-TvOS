34d3ecee0f7131a51aa5b3b6e171400b4349dbda
Aaron Parecki <aaron@parecki.com>
2015-11-18 10:15:41 -0800

    first commit

---
+++ TvOSAuth/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TvOSAuth
+//
+//  Created by Aaron Parecki on 11/14/15.
+//  Copyright © 2015 Esri. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
